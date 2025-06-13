a7e1b489f851b8828cf4579962815ac3a5871572
shuto <giaiwriting32@gmail.com>
2025-06-13 14:06:38 +0900

    feat: display tags at the bottom of the post detail page

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -9,8 +9,10 @@ import Image from 'next/image';
 import { format } from 'date-fns';
 import { ja } from 'date-fns/locale';
 import { getMarkdownTextServer } from '@/lib/markdown';
+import { Tag } from '@/types/post';
+import Link from 'next/link';
 
-type PostProp = {
+type Prop = {
   post: {
     id: number;
     title: string;
@@ -19,10 +21,11 @@ type PostProp = {
     coverImageUrl: string;
     createdAt: Date;
     updatedAt: Date;
+    tags: Tag[];
   };
 };
 
-const PostDetail = ({ post }: PostProp) => (
+const PostDetail = ({ post }: Prop) => (
   <Card className="w-full md:max-w-3xl rounded pt-0">
     <div className="relative w-full h-80 lg:h-100">
       <Image
@@ -49,6 +52,15 @@ const PostDetail = ({ post }: PostProp) => (
           className="markdown-body prose max-w-none w-full py-4"
           dangerouslySetInnerHTML={getMarkdownTextServer(post.content)}
         />
+        <div className="flex flex-wrap gap-4 px-2 mt-10">
+          {post.tags.map(tag => (
+            <Link key={tag.id} href={`/tags/${tag.name}`}>
+              <span className="text-sm px-5 py-1 font-medium text-white bg-gray-800 transition hover:bg-gray-300 hover:shadow-sm hover:scale-[1.03] rounded-full">
+                {tag.name}
+              </span>
+            </Link>
+          ))}
+        </div>
       </CardContent>
     </div>
   </Card>
