f81964a2513f8673d16775c3889b66ad0792bcb7
shuto <giaiwriting32@gmail.com>
2025-06-13 09:59:39 +0900

    style: adjust tag colors and hover styles

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -11,20 +11,16 @@ type Prop = {
 };
 
 const TagList = ({ tags }: Prop) => (
-  <div className="border rounded p-4 shadow-md">
+  <div className="border rounded bg-white p-4 shadow-md">
     <h2 className="text-lg font-bold border-b pb-2 mb-2">タグ一覧</h2>
-    <div className="grid grid-cols-4 gap-4">
+    <div className="flex flex-wrap gap-2">
       {tags.map(tag => (
         <Link
           href={`/tags/${tag.name}`}
           key={tag.id}
-          className="shadow-sm hover:shadow-md transform transition-all hover:bg-gray-50 hover:-translate-y-1 duration-300 ease-in-out"
+          className="px-3 py-1 text-sm font-medium text-gray-700 bg-gray-100 rounded-full hover:text-white btc"
         >
-          <div>
-            <p>
-              {tag.name} <span>{`(${tag.posts.length})`}</span>
-            </p>
-          </div>
+          {tag.name}
         </Link>
       ))}
     </div>
