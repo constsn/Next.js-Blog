a388793b7e499154ecf8a8893c99467c5c0faba6
shuto <giaiwriting32@gmail.com>
2025-06-27 20:01:56 +0900

    feat: center component using mx-auto

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -2,10 +2,14 @@ import { cookies } from 'next/headers';
 
 export const getOrSetAnonymousId = async () => {
   const store = await cookies();
+  // アクセスしてきた(リクエストが送られた)該当のブラウザの情報を取得
+  // そのブラウザの情報をもとに、idを持っているか確認
   let id = store.get('anonymousId')?.value;
 
   if (!id) {
     id = crypto.randomUUID();
+    // 該当のブラウザにこのcookieを保存してと指示
+    // ブラウザはこのcookieを保存 ▶️▶️次からはidを持っている
     store.set('anonymousId', id, {
       httpOnly: true,
       path: '/',
